6e8518cda6c338e3982987f3ece4757d09af7801
MapleJupiter <sunflower-beacon@protonmail.com>
2021-06-12 23:46:42 +0800

    Implement momentum in the playerChar's movement.

--- playerChar/playerChar.gd
+++ playerChar/playerChar.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var maxSpeed = 400.0 # pixels per second
-var acceleration = 10 # pixels per second^2
+export var maxSpeed = 300.0 # pixels per second
+export var accelerationScalar = 30 # pixels per second^2
 var currentVelocity = Vector2.ZERO
 var screen_size = Vector2.ZERO
 
@@ -13,25 +13,30 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	pass # Replace with function body.
 
-var velocity = Vector2()
-
 func get_input():
-	velocity = Vector2()
+	var changeInVelocity = Vector2.ZERO
+	var directionPressed = false
 	# Doing it like this allows for moving diagonally
 	# and for opposing directions to cancel each other out.
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		changeInVelocity.x += 1
+		directionPressed = true
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		changeInVelocity.x -= 1
+		directionPressed = true
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		changeInVelocity.y += 1
+		directionPressed = true
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	velocity = velocity.normalized() * maxSpeed
+		changeInVelocity.y -= 1
+		directionPressed = true
+	
+	if (directionPressed):
+		currentVelocity += (changeInVelocity.normalized() * accelerationScalar).clamped(maxSpeed)
 
 func _physics_process(_delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	currentVelocity = move_and_slide(currentVelocity.clamped(maxSpeed))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
