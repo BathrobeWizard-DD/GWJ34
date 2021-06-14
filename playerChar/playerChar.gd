dd398ec558defec356795b1d03064f3d5d9a6370
MapleJupiter <sunflower-beacon@protonmail.com>
2021-06-14 12:14:51 +0800

    Implement use of linear damp for player 'braking'.

--- playerChar/playerChar.gd
+++ playerChar/playerChar.gd
@@ -5,11 +5,12 @@ extends RigidBody2D
 # var b = "text"
 export var maxSpeed = 200.0 # pixels per second
 export var accelerationScalar = 50 # pixels per second^2
+export var brakeFactor = 10.0
 var screen_size = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_friction(0)
+	set_linear_damp(0.0)
 	screen_size = get_viewport_rect().size
 	pass # Replace with function body.
 
@@ -33,19 +34,19 @@ func get_input():
 		directionPressed = true
 	
 	if Input.is_action_pressed("click_rightbutton"):
-		changeInVelocity = linear_velocity.rotated(PI)
-		set_friction(1)
-		finalAccel = (changeInVelocity.normalized() * accelerationScalar).clamped(maxSpeed)
-		apply_central_impulse(finalAccel)
+		set_linear_damp(brakeFactor)
 	elif Input.is_action_just_released("click_rightbutton"):
-		set_friction(0)
+		set_linear_damp(0.0)
 	elif (directionPressed):
-		finalAccel = (changeInVelocity.normalized() * accelerationScalar).clamped(maxSpeed)
+		finalAccel = (changeInVelocity.normalized() * accelerationScalar)
 		apply_central_impulse(finalAccel)
 
-#func _physics_process(_delta):
-#	get_input()
+func _integrate_forces(state):
+	pass
+
+func _physics_process(_delta):
+	get_input()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	get_input()
+#func _process(_delta):
+#	get_input()
