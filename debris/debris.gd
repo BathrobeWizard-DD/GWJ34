5c2dcde3d585c53547e41f2c8dd515e5da2a3b3f
MapleJupiter <sunflower-beacon@protonmail.com>
2021-06-15 23:24:34 +0800

    Add debris scene, script, and images

---
+++ debris/debris.gd
@@ -0,0 +1,38 @@
+extends RigidBody2D
+
+export var min_speed = 150.0
+export var max_speed = 250.0
+
+var image_path_template = "res://assets/sprites/debris/debris_%d.png"
+var collisionExtents = [
+	Vector2(11.9, 9.985),
+	Vector2(14.461, 11.544),
+	Vector2(8.559, 6.978),
+	Vector2(11.677, 8.426),
+	Vector2(11.677, 8.426),
+	Vector2(11.009, 9.651)
+]
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var chosen_frame_number = (randi() % 6)
+	var image_path = image_path_template % (chosen_frame_number+1)
+	
+	var extents_properties = collisionExtents[chosen_frame_number]
+	
+	$Sprite.set_texture(load(image_path))
+	$CollisionShape2D.shape.set_extents(extents_properties)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
