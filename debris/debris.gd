6d20ced54551115ce6ad1f132e1d9836dd00156f
MapleJupiter <sunflower-beacon@protonmail.com>
2021-06-16 00:04:49 +0800

    Implement 'collision detection' between player projectiles and debris, so that debris is destroyed.

--- debris/debris.gd
+++ debris/debris.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 export var min_speed = 150.0
 export var max_speed = 250.0
 
+signal hit_by_projectile
+
 var image_path_template = "res://assets/sprites/debris/debris_%d.png"
 var collisionExtents = [
 	Vector2(11.9, 9.985),
@@ -20,6 +22,7 @@ var collisionExtents = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	add_to_group("debris")
 	var chosen_frame_number = (randi() % 6)
 	var image_path = image_path_template % (chosen_frame_number+1)
 	
@@ -36,3 +39,13 @@ func _ready():
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_debris_body_entered(body):
+	print("Something has entered debris's body:")
+	print(body)
+
+
+func _on_debris_hit_by_projectile():
+	queue_free()
+	pass # Replace with function body.
--- playerChar/playerProjectile/standardProjectile.gd
+++ playerChar/playerProjectile/standardProjectile.gd
@@ -33,3 +33,10 @@ func _physics_process(delta):
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	pass # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	if (body.is_in_group("debris")):
+		body.emit_signal("hit_by_projectile")
+		queue_free()
+	pass # Replace with function body.
