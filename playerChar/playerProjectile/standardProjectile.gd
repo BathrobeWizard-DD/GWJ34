0435fbfabb032fb598c3fc22e91dad0597be2353
MapleJupiter <sunflower-beacon@protonmail.com>
2021-06-16 21:45:57 +0800

    Finish the rest of the satellite. Bullets will disappear when hitting the satellite.

--- playerChar/playerProjectile/standardProjectile.gd
+++ playerChar/playerProjectile/standardProjectile.gd
@@ -29,14 +29,13 @@ func _physics_process(delta):
 	projVelocity = move_and_slide(projVelocity)
 #	pass
 
-
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	pass # Replace with function body.
-
 
 func _on_Area2D_body_entered(body):
 	if (body.is_in_group("mediumDebris")):
 		body.emit_signal("hit_by_projectile")
 		queue_free()
-	pass # Replace with function body.
+	elif (body.is_in_group("centerSatellite")):
+		queue_free()
