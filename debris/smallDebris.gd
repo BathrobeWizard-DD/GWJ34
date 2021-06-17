4c63291610ad7a09689baa608e9973f7e414a0a6
MapleJupiter <sunflower-beacon@protonmail.com>
2021-06-17 20:35:47 +0800

    Implement small debris scene.

---
+++ debris/smallDebris.gd
@@ -0,0 +1,55 @@
+extends RigidBody2D
+
+export var min_speed = 150.0
+export var max_speed = 250.0
+
+signal hit_by_projectile
+
+var collisionExtents = [
+	Vector2(10.489, 9.626),
+	Vector2(10.155, 7.29),
+	Vector2(8.319, 7.123),
+	Vector2(6.15, 12.129),
+	Vector2(9.487, 4.954),
+	Vector2(5.316, 3.619),
+	Vector2(7.652, 5.789),
+	Vector2(7.819, 6.122),
+	Vector2(11.657, 8.959),
+	Vector2(8.987, 6.289),
+	Vector2(8.82, 5.288),
+	Vector2(8.82, 5.288),
+]
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	add_to_group("mediumDebris")
+	var frame_count = $AnimatedSprite.get_sprite_frames().get_frame_count("default")
+	var chosen_frame_number = (randi() % frame_count)
+	
+	var extents_properties = collisionExtents[chosen_frame_number]
+	
+	$AnimatedSprite.set_frame(chosen_frame_number)
+	$CollisionShape2D.shape.set_extents(extents_properties)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_debris_body_entered(body):
+	print(body)
+
+
+func _on_debris_hit_by_projectile():
+	queue_free()
+	pass # Replace with function body.
