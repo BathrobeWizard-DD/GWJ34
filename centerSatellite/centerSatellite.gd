36c1f11f77de765c953691eaa6d84b6fc318944a
Steven <Steafmobile@gmail.com>
2021-06-19 01:16:22 +0200

    started on satellite HP

--- centerSatellite/centerSatellite.gd
+++ centerSatellite/centerSatellite.gd
@@ -1,16 +1,16 @@
 extends StaticBody2D
 
+export(int) var MAX_HP = 10
+export(int) var health = MAX_HP setget set_health
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var texture_progress = $TextureProgress
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_left"):
+		set_health(health - 1)
+		
+func set_health(value: int) -> void:
+	health = value
+	if health == 0:
+		queue_free()
+	texture_progress.value = int((float(value) / MAX_HP * 100)) % 100
