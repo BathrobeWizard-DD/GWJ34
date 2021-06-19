74307aa2cbc7c98f9542adf1eb53dd9fedb04add
Cyril Bos <cbos.pro@gmail.com>
2021-06-19 22:49:49 +0200

    added sound to button presses + moved music to main menu

--- src/ui/options/back_main_menu_button.gd
+++ src/ui/options/back_main_menu_button.gd
@@ -5,6 +5,10 @@ export(NodePath) var menu_root
 var _main_menu = load("res://levels/main_menu.tscn")
 
 
-func _on_MainMenuButton_pressed() -> void:
+func _on_AudioStreamPlayer_finished() -> void:
 	get_tree().get_root().add_child(_main_menu.instance())
 	get_node(menu_root).queue_free()
+
+
+func _on_BackMainMenuButton_button_down() -> void:
+		$PressedSound.play()
