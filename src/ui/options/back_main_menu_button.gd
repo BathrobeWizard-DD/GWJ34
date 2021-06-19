ca6f97590f0de972300a0a070ac639c1f7fd26b0
Cyril Bos <cbos.pro@gmail.com>
2021-06-19 21:23:23 +0200

    added back button to credits screen

--- src/ui/options/back_main_menu_button.gd
+++ src/ui/options/back_main_menu_button.gd
@@ -1,8 +1,10 @@
 extends Button
 
+export(NodePath) var menu_root 
+
 var _main_menu = load("res://levels/main_menu.tscn")
 
 
 func _on_MainMenuButton_pressed() -> void:
 	get_tree().get_root().add_child(_main_menu.instance())
-	get_parent().queue_free()
+	get_node(menu_root).queue_free()
