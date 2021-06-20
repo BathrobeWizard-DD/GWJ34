a81cefcc1814377e4335abab26c2018681072d26
Steven <Steafmobile@gmail.com>
2021-06-20 16:53:14 +0200

    added particles

--- playerChar/playerProjectile/ExplosionController.gd
+++ playerChar/playerProjectile/ExplosionController.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+export(PackedScene) var explosion
 signal exploded
 
 onready var radius = $Radius
@@ -15,5 +16,9 @@ func explode():
 			if "Debris" in body.name:
 				Score.score += 100 * body.score_mult
 				body.emit_signal("hit_by_projectile", get_parent().projVelocity)
+	var explosion_particles = explosion.instance()
+	get_node("/root/Node2D").call_deferred("add_child" ,explosion_particles)
+	explosion_particles.global_position = global_position
+	explosion_particles.emitting = true
 	emit_signal("exploded")
 	get_parent().queue_free()
